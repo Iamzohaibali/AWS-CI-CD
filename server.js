c100f9e3e64ed3e0cc6b7588aaee1de5e00c25ca
Iamzohaibali <j36740062@gmail.com>
2026-05-17 12:56:23 +0500

    Checking CI-CD

--- server.js
+++ server.js
@@ -6,7 +6,21 @@ app.get("/", (req, res) => {
   res.send("Production Backend");
 });
 
+app.get("/api", (req, res) => {
+  res.json({
+    success: true,
+    message: "API Working"
+  });
+});
+
+/* NEW ROUTE */
+app.get("/health", (req, res) => {
+  res.json({
+    status: "healthy",
+    uptime: process.uptime()
+  });
+});
+
 app.listen(3000, () => {
   console.log("Server running");
 });
-
